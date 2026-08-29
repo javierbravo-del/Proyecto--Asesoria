e8aa4deece8b375a5751d90210da787c42d764ea
Javier Bravo <javier.bravo@utec.edu.pe>
2026-08-29 12:32:17 -0500

    Saturday Commit

--- src/main/java/com/example/demo/usuario/repository/UserRespository.java
+++ src/main/java/com/example/demo/usuario/repository/UserRespository.java
@@ -1,11 +1,8 @@
 package com.example.demo.usuario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.usuario.app.User;
 
-@Repository
 public interface UserRespository extends JpaRepository<User, Long> {
 
     void deleteByUsername(String username);
--- src/main/java/com/example/demo/usuario/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/usuario/repository/VehicleRepository.java
@@ -1,13 +1,9 @@
 package com.example.demo.usuario.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.usuario.app.Vehicle;
 
-@Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     Vehicle save(Vehicle vehicle);
